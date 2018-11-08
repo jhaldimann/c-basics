ef11779bcb2c6905af2b14703f90f9281ee6f13e
jhaldimann <julian@praxishaldimann.ch>
2018-11-08 15:58:50 +0100

    D

---
+++ multiplication.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int x;
+    int y;
+    int z;
+    
+    printf("Enter two values:\n");
+    scanf("%d\n", &x);
+    scanf("%d", &y);
+    
+    printf("%d\n",x*y);
+    return 0;
+}

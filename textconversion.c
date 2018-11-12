47a58ac7afd69567f376cbf58e635ef7a9f0bb32
jhaldimann <julian@praxishaldimann.ch>
2018-11-12 21:25:29 +0100

    Add file scan example

---
+++ textconversion.c
@@ -0,0 +1,23 @@
+#include <stdio.h> 
+  
+int main() 
+{ 
+    FILE *fp; 
+  
+    char filename[100], c; 
+  
+    printf("Enter the filename to open \n"); 
+    scanf("%s", filename); 
+
+    fp = fopen(filename, "r"); 
+  
+    c = fgetc(fp); 
+    while (c != EOF) 
+    { 
+        printf ("%c", c); 
+        c = fgetc(fp); 
+    } 
+  
+    fclose(fp); 
+    return 0; 
+}

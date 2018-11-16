0d4794776a1cd6651f6a9143c1dc541bf864807e
Julian <julian@praxishaldimann.ch>
2018-11-16 23:20:50 +0100

    Add quicksort solution

--- quicksort.c
+++ quicksort.c
@@ -1,14 +1,63 @@
 #include <stdio.h>
 #include <string.h>
 
+static void printArray(int *myArray, int arrayLength) {
+  // loop over the elements in array
+  for(int i=0; i < arrayLength; i++) {
+    // print element at position i
+    printf("%d ", myArray[i]);
+  }
+  // print newline at the end
+  printf("\n");
+}
+// small swap method for 2 params
+void swap(int *a, int *b) {
+  int temp = *a;
+  *a = *b;
+  *b = temp;
+}
+
+int createPartition(int *myArray, int left, int right) {
+    // define pivot element --> here first element
+    int pivot = left;
+    // save pivot value in variable
+    int pivotValue = myArray[pivot];
+    while (left < right) {
+        while (myArray[left] <= pivotValue) {
+          left++;
+        }   
+        while (myArray[right] > pivotValue) {
+          right--;
+        }
+        // check if we should swap the values
+        if (left < right) {
+          // swap two values 
+          swap(&myArray[left], &myArray[right]);
+        }  
+    }
+    // swap pivot value with end element in array
+    swap(&myArray[pivot], &myArray[right]);
+    return right;
+}
+
+void quicksort(int *target, int left, int right) {
+    if (left >= right){
+      return;
+    }
+    
+    int split = createPartition(target, left, right);
+    // call the function again two times until the array is sorted 
+    quicksort(target, left, split - 1);
+    quicksort(target, split + 1, right);
+}
+
 int main () {
-    int myArray[] = {56,4232,57,268,2523,8653,356,13,425,24543};
-
-    printf("%lu\n", sizeof(myArray) / sizeof(int));
-
-    int myCounter = (sizeof(myArray) / sizeof(int)) -1;
-
-    printf("%d\n", myCounter);
-
-    return 0;
+  int myArray[] = { 2, 6, 3, 8, 5, 4, 1, 9, 7 };
+  int arrayLength = sizeof(myArray)/sizeof(myArray[0]);
+  // print values before sort
+  printArray(myArray, arrayLength);
+  // sort that stuff
+  quicksort(myArray, 0, 10-1);
+  //  print the values again but now they are sorted
+  printArray(myArray, arrayLength);
 }

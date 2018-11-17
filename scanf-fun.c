13a5347f91e4945efaf4b817e1e2e44a05af87bc
Julian <julian@praxishaldimann.ch>
2018-11-17 23:09:09 +0100

    Fix error in scanf-fun

--- scanf-fun.c
+++ scanf-fun.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
+
+/*
+* Problem here is that you cannot insert a second input
+* You will see just some random characters in the console
+*
+* Solution use a s for string in the scanf function
+*
+
+*/
 int main() {
     char x[30];
     char y[30];
-    scanf ("%[^\n]", x);
-    scanf ("%[^\n]", y);
+    scanf ("%s[^\n]", x);
+    scanf ("%s[^\n]", y);
     fprintf (stdout,"%s - %s\n",x,y);
     return 0;
 }
